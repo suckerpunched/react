0b5de10a30b34f7827579cc9bba1b43a8d7546ee
suckerpunched <suckerpunched@selfquery.dev>
2021-01-14 14:26:18 -0800

    added new modal

--- src/component/UI/Layout/Layout.js
+++ src/component/UI/Layout/Layout.js
@@ -3,15 +3,13 @@ import React from 'react';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
 import ToolBar from '../ToolBar/ToolBar';
 
-import styles from './Layout.module.css';
-
 export default function Layout(props) {
 
   return (
     <>
       <div><ToolBar/></div>
-      <div><LeftSideNav/></div>
-      <div className={styles.content}>{props.children}</div>
+      {/* <div><LeftSideNav/></div> */}
+      <div>{props.children}</div>
     </>
   )
 
--- src/container/UserPage/UserPage.js
+++ src/container/UserPage/UserPage.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as _type from '../../store/action';
 
+import Modal from '../../component/UI/Modal/Modal';
+
 export default function UserPage() {
+  const [modalVisible, setModalVisible] = useState(true);
 
-  const userFirstName = useSelector(state => state.userpage.userFirstName)
-  const userLastName = useSelector(state => state.userpage.userLastName)
-  const userCounter = useSelector(state => state.userpage.userCounter)
+  const userFirstName = useSelector(state => state.userpage.userFirstName);
+  const userLastName = useSelector(state => state.userpage.userLastName);
+  const userCounter = useSelector(state => state.userpage.userCounter);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div>{userFirstName}</div>
-      <div>{userLastName}</div>
-      <div>{userCounter}</div>
+      <Modal show={modalVisible} modalClosed={() => {setModalVisible(false)}}>
+        <div>{userFirstName}</div>
+        <div>{userLastName}</div>
+        <div>{userCounter}</div>
 
-      <button onClick={() => dispatch({ type:_type.SET_USER_FIRST_NAME, payload:'sucker' })}>first</button>
-      <button onClick={() => dispatch({ type:_type.SET_USER_LAST_NAME, payload:'punched' })}>last</button>
-      <button onClick={() => dispatch({ type:_type.INCR_USER_COUNTER })}>+</button>
+        <button onClick={() => dispatch({ type:_type.SET_USER_FIRST_NAME, payload:'sucker' })}>first</button>
+        <button onClick={() => dispatch({ type:_type.SET_USER_LAST_NAME, payload:'punched' })}>last</button>
+        <button onClick={() => dispatch({ type:_type.INCR_USER_COUNTER })}>+</button>
 
 
-      <button onClick={() => dispatch({ type:_type.CLEAR_SESSION, payload:'userpage' })}>clear session</button>
-      <button onClick={() => dispatch({ type:_type.SAVE_SESSION, payload:'userpage' })}>save session</button>
-      <button onClick={() => dispatch({ type:_type.LOAD_SESSION, payload:'userpage' })}>load session</button>
+        <button onClick={() => dispatch({ type:_type.CLEAR_SESSION, payload:'userpage' })}>clear session</button>
+        <button onClick={() => dispatch({ type:_type.SAVE_SESSION, payload:'userpage' })}>save session</button>
+        <button onClick={() => dispatch({ type:_type.LOAD_SESSION, payload:'userpage' })}>load session</button>
+      </Modal>
     </>
   )
   
